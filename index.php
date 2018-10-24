34af7d9d283934a2bf3d38c025a8a1cb3e0a9963
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-25 11:02:47 +1300

    index.php loads individual post data

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 
-		<title></title>
-
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<h1>This is the custom theme</h1>
+		<?php if(have_posts()): ?>
+			<?php while(have_posts()): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<div><?php the_content(); ?></div>
+			<?php endwhile; ?>
+		<?php endif; ?>
 
 		<?php wp_footer(); ?>
 	</body>
