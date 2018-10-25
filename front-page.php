d3ed52eac3ffac9dcef0cb081567c771ce3b8070
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-26 10:57:32 +1300

    Added front-page.php and improved layout

--- front-page.php
+++ front-page.php
@@ -9,6 +9,12 @@
 	</head>
 	<body <?php body_class(); ?>>
 		<div class="container mt-2">
+			<div class="row">
+				<div class="col">
+					<h1>Home Page</h1>
+				</div>
+			</div>
+
 			<?php if(have_posts()): ?>
 				<?php while(have_posts()): the_post(); ?>
 					<div class="card text-light bg-secondary mt-2">
