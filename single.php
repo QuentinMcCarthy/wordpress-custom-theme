cf8df70bf29501327d546e81c8c239fcbf98339c
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-26 11:26:57 +1300

    Single post page

---
+++ single.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+
+		<title><?php the_title(); ?></title>
+
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<div class="container mt-2">
+			<?php if(have_posts()): ?>
+				<?php while(have_posts()): the_post(); ?>
+					<div class="card text-light bg-secondary">
+						<?php if(has_post_thumbnail()): ?>
+							<?php the_post_thumbnail('large', ['class'=>'card-img-top']); ?>
+
+							<div class="card-body">
+								<h5 class="card-title"><?php the_title(); ?></h5>
+								<p class="card-text"><?php the_content(); ?></p>
+							</div>
+						<?php else: ?>
+							<div class="card-header"><?php the_title(); ?></div>
+
+							<div class="card-body">
+								<p><?php the_content(); ?></p>
+							</div>
+						<?php endif; ?>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
