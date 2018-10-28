63ae3602b01f3069887804306f13737ec56af542
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-29 10:10:59 +1300

    Working on navigation styles; waiting on walker

--- header.php
+++ header.php
@@ -8,6 +8,24 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<?php // if(has_nav_menu('defaultnav')): ?>
+			<!-- <nav class="navbar navbar-dark bg-dark"> -->
+				<?php
+					// wp_nav_menu(array(
+					// 	'theme_location'=>'defaultnav',
+					// 	'container_class'=>'navbar-collapse',
+					// 	'menu_class'=>'navbar-nav mr-auto',
+					// 	'menu_id'=>'defaultnav'
+					// ));
+				?>
+
+				<!-- <form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</nav> -->
+		<?php // endif; ?>
+
 		<?php
 			if(has_nav_menu('defaultnav')){
 				wp_nav_menu(array(
