2c66cadfd32d6eb3867e2c8c038afab73d77ac39
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-30 08:59:34 +1300

    Fixed indentation and code layout

--- front-page.php
+++ front-page.php
@@ -1,34 +1,33 @@
 <?php get_header( 'front' ); ?>
-		<div class="container mt-2">
-			<div class="row">
-				<div class="col">
-					<h1>Home Page</h1>
-				</div>
+	<div class="container mt-2">
+		<div class="row">
+			<div class="col">
+				<h1>Home Page</h1>
 			</div>
-
-			<?php if ( have_posts() ): ?>
-				<?php while ( have_posts() ): the_post(); ?>
-					<div class="card text-light bg-secondary mt-2">
-						<div class="card-header"><?php the_title(); ?></div>
-						<div class="card-body row">
-							<?php if ( has_post_thumbnail() ): ?>
-								<?php $colClass = 'col-md-8' ?>
-
-								<div class="col-md-4">
-									<?php the_post_thumbnail( 'medium', ['class' => 'img-fluid'] ); ?>
-								</div>
-							<?php else: ?>
-								<?php $colClass = 'col' ?>
-							<?php endif; ?>
-
-							<div class="<?php echo $colClass ?>">
-								<div class="card-text"><?php the_excerpt(); ?></div>
-								<a class="btn btn-primary" href="<?php echo esc_url(get_permalink()); ?>">Read more...</a>
-							</div>
-						</div>
-					</div>
-				<?php endwhile; ?>
-			<?php endif; ?>
 		</div>
 
-		<?php get_footer(); ?>
+		<?php if ( have_posts() ): ?>
+			<?php while ( have_posts() ): the_post(); ?>
+				<div class="card text-light bg-secondary mt-2">
+					<div class="card-header"><?php the_title(); ?></div>
+					<div class="card-body row">
+						<?php if ( has_post_thumbnail() ): ?>
+							<?php $colClass = 'col-md-8' ?>
+
+							<div class="col-md-4">
+								<?php the_post_thumbnail( 'medium', ['class' => 'img-fluid'] ); ?>
+							</div>
+						<?php else: ?>
+							<?php $colClass = 'col' ?>
+						<?php endif; ?>
+
+						<div class="<?php echo $colClass ?>">
+							<div class="card-text"><?php the_excerpt(); ?></div>
+							<a class="btn btn-primary" href="<?php echo esc_url(get_permalink()); ?>">Read more...</a>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,25 +1,24 @@
 <?php get_header(); ?>
-		<div class="container mt-2">
-			<?php if (have_posts()): ?>
-				<?php while ( have_posts() ): the_post(); ?>
-					<div class="card text-light bg-secondary">
-						<?php if (has_post_thumbnail()): ?>
-							<?php the_post_thumbnail( 'large', ['class' => 'card-img-top'] ); ?>
+	<div class="container mt-2">
+		<?php if (have_posts()): ?>
+			<?php while ( have_posts() ): the_post(); ?>
+				<div class="card text-light bg-secondary">
+					<?php if (has_post_thumbnail()): ?>
+						<?php the_post_thumbnail( 'large', ['class' => 'card-img-top'] ); ?>
 
-							<div class="card-body">
-								<h5 class="card-title"><?php the_title(); ?></h5>
-								<p class="card-text"><?php the_content(); ?></p>
-							</div>
-						<?php else: ?>
-							<div class="card-header"><?php the_title(); ?></div>
+						<div class="card-body">
+							<h5 class="card-title"><?php the_title(); ?></h5>
+							<p class="card-text"><?php the_content(); ?></p>
+						</div>
+					<?php else: ?>
+						<div class="card-header"><?php the_title(); ?></div>
 
-							<div class="card-body">
-								<p><?php the_content(); ?></p>
-							</div>
-						<?php endif; ?>
-					</div>
-				<?php endwhile; ?>
-			<?php endif; ?>
-		</div>
-
-		<?php get_footer(); ?>
+						<div class="card-body">
+							<p><?php the_content(); ?></p>
+						</div>
+					<?php endif; ?>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+<?php get_footer(); ?>
