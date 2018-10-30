670b37629264c2ff4bb2097a625445bafc2a4dba
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-31 11:59:03 +1300

    In-progress addition of carousels

--- functions.php
+++ functions.php
@@ -45,9 +45,9 @@
 			'add_new_item'       => _x( 'Add New Staff Member', 'Adding new staff member', '18wdwu02theme' ),
 			'edit_item'          => _x( 'Edit Staff Member', 'Editing staff member', '18wdwu02theme' ),
 			'new_item'           => _x( 'New Staff Member', 'New staff member', '18wdwu02theme' ),
-			'view_item'          => _x( 'View Staff Member', 'View staff member', '18wdwu02theme' ),
-			'view_items'         => _x( 'View Staff Members', 'View staff members', '18wdwu02theme' ),
-			'search_items'       => _x( 'Search Staff Members', 'Search staff members', '18wdwu02theme' ),
+			'view_item'          => _x( 'View Staff Member', 'Viewing staff member', '18wdwu02theme' ),
+			'view_items'         => _x( 'View Staff Members', 'Viewing staff members', '18wdwu02theme' ),
+			'search_items'       => _x( 'Search Staff Members', 'Searching staff members', '18wdwu02theme' ),
 			'not_found'          => _x( 'No Staff Members Found', 'No staff members found', '18wdwu02theme' ),
 			'not_found_in_trash' => _x( 'No Staff Members found in Trash', 'No Staff Members found in Trash', '18wdwu02theme' ),
 			'all_items'          => _x( 'All Staff Members', 'All staff members', '18wdwu02theme' ),
@@ -76,11 +76,50 @@
 		register_post_type( 'staff', $args);
 	}
 
+	function add_carousel_post_type(){
+		$labels = array(
+			'name'               => _x( 'Carousels', 'Post type name', '18wdwu02theme' ),
+			'singular_name'      => _x( 'Carousel', 'Post type singular name', '18wdwu02theme' ),
+			'add_new'            => _x( 'Create Carousel', 'Creating new carousel', '18wdwu02theme' ),
+			'add_new_item'       => _x( 'Create New Carousel', 'Creating new carousel', '18wdwu02theme' ),
+			'edit_item'          => _x( 'Edit Carousel', 'Editing carousel', '18wdwu02theme' ),
+			'new_item'           => _x( 'New Carousel', 'New carousel', '18wdwu02theme' ),
+			'view_item'          => _x( 'View Carousel', 'Viewing carousel', '18wdwu02theme' ),
+			'view_items'         => _x( 'View Carousels', 'Viewing carousels', '18wdwu02theme' ),
+			'search_items'       => _x( 'Search Carousels', 'Searching carousels', '18wdwu02theme' ),
+			'not_found'          => _x( 'No Carousels Found', 'No carousels found', '18wdwu02theme' ),
+			'not_found_in_trash' => _x( 'No Carousels found in Trash', 'No carousels found in trash', '18wdwu02theme' ),
+			'all_items'          => _x( 'All Carousels', 'All carousels', '18wdwu02theme' ),
+		);
+
+		$supports = array(
+			'title',
+			'thumbnail',
+		);
+
+		$args = array(
+			'labels'              => $labels,
+			'description'         => 'A post type for carousels on thw website',
+			'public'              => true,
+			'hierarchical'        => true,
+			'exclude_from_search' => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'show_in_nav_menus'   => false,
+			'menu_position'       => 20,
+			'menu_icon'           => 'dashicons-images-alt2',
+			'supports'            => $supports,
+			'query_var'           => true,
+		);
+
+		register_post_type( 'carousel', $args);
+	}
 
 	// 3.0 Actions
 	add_action( 'wp_enqueue_scripts', 'add_custom_files' );
 	add_action( 'init', 'add_custom_menus' );
 	add_action( 'init', 'add_staff_post_type' );
+	add_action( 'init', 'add_carousel_post_type' );
 
 
 	// 4.0 Theme Support
