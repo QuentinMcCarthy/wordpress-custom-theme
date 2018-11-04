bcaff0c6fe7b1ad26332a68b3b57a79b73e6003a
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-05 09:31:28 +1300

    Added sidebar & widgets

--- front-page.php
+++ front-page.php
@@ -6,37 +6,49 @@
 			</div>
 		</div>
 
-		<?php if ( have_posts() ): ?>
-			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-				<ol class="carousel-indicators">
-					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-				</ol>
-				<div class="carousel-inner">
-					<div class="carousel-item active">
-						<img class="d-block w-100" src="http://placehold.it/800x400" alt="First slide">
+		<div class="row">
+			<div class="col">
+				<?php if ( have_posts() ): ?>
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img class="d-block w-100" src="http://placehold.it/800x400" alt="First slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="http://placehold.it/800x400" alt="Second slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="http://placehold.it/800x400" alt="Third slide">
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
 					</div>
-					<div class="carousel-item">
-						<img class="d-block w-100" src="http://placehold.it/800x400" alt="Second slide">
-					</div>
-					<div class="carousel-item">
-						<img class="d-block w-100" src="http://placehold.it/800x400" alt="Third slide">
-					</div>
-				</div>
-				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
+
+					<?php while ( have_posts() ): the_post(); ?>
+						<?php get_template_part( 'content', get_post_format() ); ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
 			</div>
 
-			<?php while ( have_posts() ): the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-		<?php endif; ?>
+			<?php if ( is_active_sidebar('sidebar-main') ): ?>
+				<div class="col-4">
+					<div id="sidebar-main" class="card text-light bg-secondary p-2">
+						<?php dynamic_sidebar( 'sidebar-main' ); ?>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
 	</div>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,23 +1,23 @@
 <?php
 	// Table of Contents:
 	// 1.0 Plugins
-	// 2.0 Functions for Actions
+	// 2.0 Theme Actions
 	// 2.1 Stylesheets and Scripts
 	// 2.2 Menus
-	// 2.3 Custom Post Types
-	// 3.0 Actions
-	// 4.0 Theme Support
-	// 5.0 Other
+	// 2.3 Sidebars
+	// 2.4 Custom Post Types
+	// 3.0 Theme Support
+	// 4.0 Other
 
 
 	// 1.0 Plugins
 	require_once get_template_directory().'/class-wp-bootstrap-navwalker.php';
 
 
-	// 2.0 Functions for Actions
+	// 2.0 Theme Actions
 
 	// 2.1 Stylesheets and Scripts
-	function add_custom_files() {
+	add_action( 'wp_enqueue_scripts', function() {
 		$css_directory = get_template_directory_uri().'/assets/css/';
 		$js_directory = get_template_directory_uri().'/assets/js/';
 
@@ -29,15 +29,28 @@
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'bootstrapjs', $js_directory.'bootstrap.min.js', array(), '4.1.3', true );
 		wp_enqueue_script( 'master', $js_directory.'script.js', array( 'jquery' ), '0.0.1', true );
-	}
+	});
 
 	// 2.2 Menus
-	function add_custom_menus() {
+	add_action( 'init', function() {
 		// Default navigation menu
 		register_nav_menu( 'defaultnav', __( 'Default Navigation' ) );
-	}
+	});
 
-	// 2.3 Custom Post Types
+	// 2.3 Sidebars
+	add_action( 'widgets_init', function() {
+		register_sidebar(array(
+			'id'            => 'sidebar-main',
+			'name'          => __( 'Main Sidebar', 'theme-slug' ),
+			'description'   => __( 'Main sidebar appears on all pages', 'theme-slug' ),
+			'before_widget' => '<div id="%1$s" class="custom-widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		));
+	});
+
+	// 2.4 Custom Post Types
 	function add_staff_post_type(){
 		$labels = array(
 			'name'               => _x( 'Staff', 'Post type name', '18wdwu02theme' ),
@@ -115,14 +128,11 @@
 		register_post_type( 'carousel', $args);
 	}
 
-	// 3.0 Actions
-	add_action( 'wp_enqueue_scripts', 'add_custom_files' );
-	add_action( 'init', 'add_custom_menus' );
 	add_action( 'init', 'add_staff_post_type' );
 	add_action( 'init', 'add_carousel_post_type' );
 
 
-	// 4.0 Theme Support
+	// 3.0 Theme Support
 	add_theme_support( 'post-thumbnails' );
 
 	$post_formats = array(
@@ -135,5 +145,5 @@
 	add_theme_support( 'post-formats', $post_formats );
 
 
-	// 5.0 Other
+	// 4.0 Other
 	add_image_size( 'icon', 50, 50, true );
