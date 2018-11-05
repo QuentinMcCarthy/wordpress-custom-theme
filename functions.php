26a715b69bff1234fdefe55835003bea263c5c0d
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-06 09:22:10 +1300

    Fixed table of contents; adding custom background support

--- functions.php
+++ functions.php
@@ -1,13 +1,17 @@
 <?php
 	/* Table of Contents:
 		1.0:- Plugins
-		2.0:- Theme Actions
-			2.1:- Stylesheets and Scripts
-			2.2:- Menus
-			2.3:- Sidebars
-			2.4:- Custom Post Types
-		3.0:- Theme Support
-		4.0:- Other
+		2.0:- Stylesheets and Scripts
+		3.0:- Menus
+		4.0:- Sidebars
+		5.0:- Custom Post Types
+			5.1:- Staff
+			5.2:- Carousel
+		6.0:- Custom Header
+		7.0:- Custom Background
+		8.0:- Post Thumbnails
+		9.0:- Post Formats
+		10.0:- Other
 	*/
 
 
@@ -15,9 +19,7 @@
 	require_once get_template_directory().'/class-wp-bootstrap-navwalker.php';
 
 
-	// 2.0:- Theme Actions
-
-	// 2.1:- Stylesheets and Scripts
+	// 2.0:- Stylesheets and Scripts
 	add_action( 'wp_enqueue_scripts', function() {
 		$css_directory = get_template_directory_uri().'/assets/css/';
 		$js_directory = get_template_directory_uri().'/assets/js/';
@@ -32,13 +34,15 @@
 		wp_enqueue_script( 'master', $js_directory.'script.js', array( 'jquery' ), '0.0.1', true );
 	});
 
-	// 2.2:- Menus
+
+	// 3.0:- Menus
 	add_action( 'init', function() {
 		// Default navigation menu
 		register_nav_menu( 'defaultnav', __( 'Default Navigation' ) );
 	});
 
-	// 2.3:- Sidebars
+
+	// 4.0:- Sidebars
 	add_action( 'widgets_init', function() {
 		register_sidebar(array(
 			'id'            => 'sidebar-main',
@@ -51,8 +55,10 @@
 		));
 	});
 
-	// 2.4:- Custom Post Types
-	function add_staff_post_type(){
+	// 5.0:- Custom Post Types
+
+	// 5.1:- Staff
+	function add_staff_post_type() {
 		$labels = array(
 			'name'               => _x( 'Staff', 'Post type name', '18wdwu02theme' ),
 			'singular_name'      => _x( 'Staff', 'Post type singular name', '18wdwu02theme' ),
@@ -90,7 +96,8 @@
 		register_post_type( 'staff', $args);
 	}
 
-	function add_carousel_post_type(){
+	// 5.2:- Carousel
+	function add_carousel_post_type() {
 		$labels = array(
 			'name'               => _x( 'Carousels', 'Post type name', '18wdwu02theme' ),
 			'singular_name'      => _x( 'Carousel', 'Post type singular name', '18wdwu02theme' ),
@@ -156,11 +163,29 @@
 			'default-text-color'     => '',
 		);
 
-	// 3.0:- Theme Support
 		add_theme_support( 'custom-header', $defaults );
 	});
+
+
+	// 7.0:- Custom Background
+	$custom_background_defaults = array(
+		'default-color'          => '000000',
+		'default-image'          => '',
+		'default-repeat'         => 'repeat',
+		'default-position-x'     => 'left',
+        'default-position-y'     => 'top',
+        'default-size'           => 'auto',
+		'default-attachment'     => 'fixed',
+	);
+
+	add_theme_support( 'custom-background', $custom_background_defaults );
+
+
+	// 8.0:- Post Thumbnails
 	add_theme_support( 'post-thumbnails' );
 
+
+	// 9.0 Post Formats
 	$post_formats = array(
 		'aside',
 		'gallery',
@@ -171,5 +196,5 @@
 	add_theme_support( 'post-formats', $post_formats );
 
 
-	// 4.0:- Other
+	// 10.0:- Other
 	add_image_size( 'icon', 50, 50, true );
