436fe99339f80d37ccc66737655d2c2915485a5a
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-23 09:47:33 +1300

    Change single quotes in js to double quotes

--- assets/js/theme-scripts.js
+++ assets/js/theme-scripts.js
@@ -2,14 +2,14 @@
 $ = jQuery;
 
 $(document).ready(function(){
-	// alert('JS: Ready');
+	// alert("JS: Ready");
 
-	$('.show-more').click(function(){
+	$(".show-more").click(function(){
 		let button = $(this);
 		let data = {
-			'action': 'loadmore',
-			'query': front_page_show_more.query,
-			'page': front_page_show_more.current_page,
+			"action": "loadmore",
+			"query": front_page_show_more.query,
+			"page": front_page_show_more.current_page,
 		};
 
 		$.ajax({
@@ -17,13 +17,13 @@ $(document).ready(function(){
 			url: front_page_show_more.ajax_url,
 			data: data,
 			beforeSend: function(xhr){
-				button.text('Loading...');
+				button.text("Loading...");
 			},
 			success: function(data){
 				if(data){
-					button.text( 'Show More' );
+					button.text( "Show More" );
 
-					$('.front-page-posts').append(data);
+					$(".front-page-posts").append(data);
 
 					front_page_show_more.current_page++;
 
